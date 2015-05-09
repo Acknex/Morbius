d56607668e33d569edd288290eb8f851f72c6f7e
Felix Queißner <felix@mq32.de>
2015-05-09 02:00:51 +0200

    Adds menu prototype music.

--- source/src/menu.c
+++ source/src/menu.c
@@ -1,6 +1,18 @@
 #include "menu.h"
 
+STRING *msDiscoMusic = "media\\Sumik_dj_-_wigi.mp3";
+
+typedef struct {
+    var music;
+
+
+} MenuData;
+
+MenuData _menu;
+
 void menu_open()
 {
     level_load(NULL);
+
+    _menu.music = media_loop(msDiscoMusic, NULL, 100);
 }
