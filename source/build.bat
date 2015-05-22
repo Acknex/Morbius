4fa8eebf9b9deeb554eb7fa9f88191deb3ad41cf
firoball <unsinnspala@firoball.de>
2015-05-22 16:56:56 +0200

    exclude media folder from wrs with build hack

--- source/build.bat
+++ source/build.bat
@@ -1,5 +1,5 @@
 @echo off
-REM adjust path to engine
+rem adjust path to engine
 Set "AckPath=d:\acknex8" 
 
 Set "path=%path%;%AckPath%" 
@@ -10,13 +10,19 @@ acknex create_bindings.c
 
 echo.
 echo Create Resources and Executable...
-WED -r main.c
-REM WED -p main.c
+
+rem options.scr does not allow to ignore certain folders so provoke 
+rem file not found for everything in "media" with folder renaming
+
+ren media media_renamed_by_buildscript
+wed -r main.c
+rem wed -p main.c
+ren media_renamed_by_buildscript media
 
 echo.
 echo Copy unresourced files...
 xcopy /y media\*.* main.cd\media\
-xcopy /y main.cd\*.xml main.cd\xml\
+xcopy  main.cd\*.xml main.cd\xml\
 del main.cd\*.xml
 del main.exe
 
