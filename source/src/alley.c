c4aae0f9c103463d305042a13340af1ef82f01b4
Lemming <user@host.domain>
2015-05-26 21:40:55 +0200

    Fixing Alley scale.

--- source/src/alley.c
+++ source/src/alley.c
@@ -9,6 +9,8 @@ void alley_fade_p(PARTICLE* p)
 {
 	p.alpha -= p.skill_a*time_step;
 	if (p.alpha <= 0) p.lifespan = 0;
+	
+	p.size = minv(p.skill_b, p.size+time_step*5);
 }
 
 BMAP* bmp_alley_smoke = "rauch2.tga";
@@ -18,8 +20,9 @@ void alley_smoke_p(PARTICLE* p)
 	p.skill_a = 3;
 	p.bmap = bmp_alley_smoke;
 	p.lifespan = 1200+random(600);
-	p.size = 60 + random(40);
-	p.alpha = 128;
+	p.skill_b = 20 + random(15);
+	p.size = 0;
+	p.alpha = 64;
 	vec_scale(p.vel_x, 0.4);
 	set(p, TRANSLUCENT | MOVE | BRIGHT);
 	p.event = alley_fade_p;
@@ -31,9 +34,9 @@ action alley_smoke(void)
 	var partout = 0;
 	while (1)
 	{
-		partout += time_step/8;
+		partout += time_step/3;
 		effect(alley_smoke_p, partout,
-		_vec(me.x+random(20)-10, me.y+random(20)-10, me.z+random(20)-10),
+		_vec(me.x+random(10)-5, me.y+random(10)-5, me.z+random(10)-5),
 		_vec(-5-random(10), -2-random(4), 2+random(4)));
 		partout -= integer(partout);
 		wait(1);
