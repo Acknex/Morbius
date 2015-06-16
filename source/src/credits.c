7cb86173c159cec634074bf5a505e61b7a4c2919
Felix Queißner <git@mq32.de>
2015-06-16 21:01:08 +0200

    More credits.

--- source/src/credits.c
+++ source/src/credits.c
@@ -2,6 +2,7 @@
 #include "math.h"
 
 BMAP *bmpCreditsText = "credits.png";
+BMAP *bmpCreditsLogo = "morbius.png";
 STRING *msCreditsMusic = "media\\credits.ogg";
 
 struct
@@ -49,22 +50,41 @@ void credits_core()
 	
 	wait(1);
 	
-	_credits.scroll = -screen_size.y;
+	_credits.scroll = -1.1 * screen_size.y;
 	while(1)
 	{
-		float scale = 1.2;
-		float aspect = screen_size.y / screen_size.x;
-		int x = (screen_size.x - scale * bmpCreditsText.width) / 2;
-		draw_quad(
-			bmpCreditsText,
-			vector(x, -minv(_credits.scroll, 0), 0),
-			vector(0, maxv(_credits.scroll, 0), 0),
-			vector(bmpCreditsText.width, screen_size.y / scale, 0),
-			vector(scale, scale, 1),
-			COLOR_WHITE,
-			100,
-			0);
-		
+		if(_credits.scroll < -0.75 * screen_size.y) {
+			
+			var alpha = clamp(4 * (abs(screen_size.y) - abs(_credits.scroll)), 0, 100);
+			
+			draw_quad(
+				bmpCreditsLogo,
+				vector(
+					(screen_size.x - bmpCreditsLogo.width) / 2,
+					(screen_size.y - bmpCreditsLogo.height) / 2,
+					0),
+				NULL,
+				NULL,
+				NULL,
+				COLOR_WHITE,
+				100 - alpha,
+				0);
+		}
+		if(_credits.scroll >= -screen_size.y) {
+			float scale = 1.2;
+			float aspect = screen_size.y / screen_size.x;
+			int x = (screen_size.x - scale * bmpCreditsText.width) / 2;
+			draw_quad(
+				bmpCreditsText,
+				vector(x, -minv(_credits.scroll, 0), 0),
+				vector(0, maxv(_credits.scroll, 0), 0),
+				vector(bmpCreditsText.width, screen_size.y / scale, 0),
+				vector(scale, scale, 1),
+				COLOR_WHITE,
+				100,
+				0);
+		}
+			
 		_credits.scroll = minv(_credits.scroll + 2 * time_step, bmpCreditsText.height);
 		
 		if(_credits.scroll == bmpCreditsText.height)
